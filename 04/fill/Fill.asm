066ef91fb94cc7307831660f6c27ba788451b69d
telwell <me@trevorelwell.com>
2017-09-04 19:06:47 -0700

    completing project 04. Not the best assembly but it works!

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -6,9 +6,72 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(BEGIN)
+  @KBD // 24576 in RAM
+  D=M
+  @NO_PRESS
+  D;JEQ // Goto no press if D == 0
+  @R1
+  M=-1
+  @INIT_LOOP
+  0;JMP
+
+(NO_PRESS)
+  @R1
+  M=0 // Set R1 to our desired value of 0 == false
+
+(INIT_LOOP)
+  @SCREEN
+  D=A // Set D to the beginning of SCREEN
+  @R2
+  M=D // R2 will be our current pixel pointer
+
+  @256
+  D=A
+  @R3
+  M=D // R3 == 256. Our row counter
+
+  @32
+  D=A
+  @R4
+  M=D // R4 == 32. Our pixel column counter
+
+(PIXEL_LOOP)
+  @R1
+  D=M // -1 or 0, hopefully
+  @R2
+  A=M // Go to the address stored at R2
+  M=D // Set all pixels in word to black
+
+  @R2
+  M=M+1 // Increment pixel counter
+
+  @R4
+  M=M-1 // Decrement R4, pixel column counter
+  D=M
+  @DECREMENT_ROW_COUNTER
+  D;JEQ
+
+  @PIXEL_LOOP
+  0;JMP
+
+(DECREMENT_ROW_COUNTER)
+  @32
+  D=A
+  @R4
+  M=D // Reset R4
+
+  @R3
+  M=M-1 // Decrement row loop
+  D=M
+
+  @BEGIN
+  D;JEQ
+
+  @PIXEL_LOOP
+  0;JMP
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,76 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// Setup our negative flag
+@R3
+M=-1
+
+// First let's test for zero
+(ZERO)
+  @R0 // Test if R0 == 0
+  D=M
+  @IS_ZERO
+  D;JEQ
+
+  @R1 // Test if R0 == 0
+  D=M
+  @IS_ZERO
+  D;JEQ
+
+// Now look for negative inputs
+(NEGS)
+  @R0 // Test if R0 < 0
+  D=M
+  @R0_IS_NEG
+  D;JLT
+
+(NEGS_CONT)
+  @R1 // Test if R1 < 0
+  D=M
+  @R1_IS_NEG
+  D;JLT
+  @PRE_LOOP
+  0;JMP
+
+(R0_IS_NEG)
+  @R0
+  M = -M
+  @R3
+  M = M+1 // Increment R3
+  @NEGS_CONT
+  0;JMP
+
+(R1_IS_NEG)
+  @R1
+  M = -M
+  @R3
+  M = M+1 // Increment R3
+
+// Setup the Loop
+(PRE_LOOP)
+  @R2
+  M=0
+
+// Otherwise begin the loop
+(LOOP)
+  @R0
+  D=M
+  @R2
+  M=M+D
+  @R1
+  M=M-1
+  D=M
+  @LOOP
+  D;JGT
+  @END
+  0;JMP
+
+// one of the inputs is zero so set R2 to 0 and END
+(IS_ZERO)
+  @R2
+  M=0
+
+// Infinite loop to end the program
+(END)
+  @END
+  0;JMP
